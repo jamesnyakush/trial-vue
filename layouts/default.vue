d7f5e5efa08ce0ea69cdb99b3d0857ffc1c07732
jamesnyakush <emvesttech@gmail.com>
2021-02-05 20:19:29 +0300

    First commit :rocket:

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-app>
+		<core-app-bar />
+		<core-drawer />
+		<core-view />
+	</v-app>
+</template>
+
+<script>
+import Drawer from '@/components/core/Drawer';
+import AppBar from '@/components/core/AppBar';
+import View from '@/components/core/View';
+export default {
+	props: {
+		source: String,
+	},
+	data: () => ({
+		drawer: null,
+	}),
+	components: {
+		CoreDrawer: Drawer,
+		CoreAppBar: AppBar,
+		CoreView: View,
+	},
+};
+</script>
